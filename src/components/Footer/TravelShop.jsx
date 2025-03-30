ad1c0ea28ee220be50bae135fde9f6fc4caf56e7
Hamza Bin Shahid <133975358+justhamzashahid@users.noreply.github.com>
2025-03-30 22:19:41 +0500

    travel shop center

--- src/components/Footer/TravelShop.jsx
+++ src/components/Footer/TravelShop.jsx
@@ -339,7 +339,7 @@ const TravelShop = () => {
       >
         <div className="absolute inset-0 bg-black/40"></div>
         <div className="absolute inset-0 flex items-center justify-center">
-          <h1 className="text-4xl md:text-6xl font-bold text-white">Welcome to our travelshop</h1>
+          <h1 className="text-4xl text-center md:text-6xl font-bold text-white">Welcome to our travelshop</h1>
         </div>
       </div>
       
