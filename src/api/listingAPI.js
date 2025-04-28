aece9b47d1e41f068155469e94f891ac1fdb324a
Muhammad Waqas Shahid Khan <mwaqask8040@gmail.com>
2025-04-28 22:31:03 +0500

    Resolved Error

--- src/api/listingAPI.js
+++ src/api/listingAPI.js
@@ -2,7 +2,7 @@
 import API from './config';
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || '';
+const API_URL = import.meta.env.VITE_API_URL || '';
 
 // Get all listings
 export const getAllListings = async () => {
