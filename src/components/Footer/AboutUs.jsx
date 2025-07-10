0559af34672ed4668200ca01c333d795130920bf
zubair arif <104222183+ZubairArif234@users.noreply.github.com>
2025-07-10 20:07:21 +0500

    fix(images)

--- src/components/Footer/AboutUs.jsx
+++ src/components/Footer/AboutUs.jsx
@@ -28,7 +28,7 @@ const AboutUs = () => {
         <section className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <h1 className="text-3xl md:text-5xl font-bold text-center mb-10">{t('about_us')}</h1>
 
-<div className='grid grid-cols-2 items-start '>
+<div className='grid md:grid-cols-2 gap-4 items-start '>
 
         {/* About Text */}
         <div className=" mx-auto mb-12 text-gray-700">
